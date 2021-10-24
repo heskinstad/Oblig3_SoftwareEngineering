18fde2d01fd5d11cbd60767398de1f075b929f19
Hans Skinstad <heskinstad@gmail.com>
2021-10-24 12:37:30 +0200

    Laster opp filer fra oblig 2

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    static boolean isLeapYear(int year) {
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 && year % 4000 != 0) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/test/java/CheckLeapYear.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CheckLeapYear {
+
+    @Test
+    public void Check_Dividable_By_4_Not_100() {
+        assertEquals(true, Main.isLeapYear(104));
+    }
+
+    @Test
+    public void Check_Dividable_By_400() {
+        assertEquals(true, Main.isLeapYear(2000));
+    }
+
+    @Test
+    public void Check_Not_Dividable_By_4() {
+        assertEquals(false, Main.isLeapYear(103));
+    }
+
+    @Test
+    public void Check_Dividable_By_4_And_100() {
+        assertEquals(false, Main.isLeapYear(1900));
+    }
+
+    @Test
+    public void Check_Dividable_By_4000() {
+        assertEquals(false, Main.isLeapYear(8000));
+    }
+}
