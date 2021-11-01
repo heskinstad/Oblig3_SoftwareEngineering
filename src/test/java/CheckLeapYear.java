f721782bcf57c4de2d77625acf20f32329405646
Hans Skinstad <heskinstad@gmail.com>
2021-11-01 11:46:59 +0100

    test

--- src/test/java/CheckLeapYear.java
+++ src/test/java/CheckLeapYear.java
@@ -4,27 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CheckLeapYear {
 
     @Test
-    public void test_Check_Dividable_By_4_Not_100() {
+    public void Test_Check_Dividable_By_4_Not_100() {
         assertEquals(true, Main.isLeapYear(104));
     }
 
     @Test
-    public void test_Check_Dividable_By_400() {
+    public void Test_Check_Dividable_By_400() {
         assertEquals(true, Main.isLeapYear(2000));
     }
 
     @Test
-    public void test_Check_Not_Dividable_By_4() {
+    public void Test_Check_Not_Dividable_By_4() {
         assertEquals(true, Main.isLeapYear(103));
     }
 
     @Test
-    public void test_Check_Dividable_By_4_And_100() {
+    public void Test_Check_Dividable_By_4_And_100() {
         assertEquals(false, Main.isLeapYear(1900));
     }
 
     @Test
-    public void test_Check_Dividable_By_4000() {
+    public void Test_Check_Dividable_By_4000() {
         assertEquals(false, Main.isLeapYear(8000));
     }
 }
